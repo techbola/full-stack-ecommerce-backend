5e70f590bc1b0c915cd05b4b8feb1b0a16b48403
Adebola <adebola@poptin.com>
2024-06-19 17:43:59 +0100

    add user product to cart, remove user product from cart using mongodb

--- src/server.js
+++ src/server.js
@@ -1,69 +1,99 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 
-import {
-  cartItems as cartItemsRaw,
-  products as productsRaw,
-} from "./temp-data";
+// import {
+//   cartItems as cartItemsRaw,
+//   products as productsRaw,
+// } from "./temp-data";
 
 // so we can modify the external data
-let cartItems = cartItemsRaw;
-let products = productsRaw;
+// let cartItems = cartItemsRaw;
+// let products = productsRaw;
 
-const url = `mongodb+srv://techbolaf:bi23VitzFnwdJVDi@cluster0.yn3tllk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const client = new MongoClient(url);
+async function start() {
+  const url = `mongodb+srv://techbolaf:bi23VitzFnwdJVDi@cluster0.yn3tllk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const client = new MongoClient(url);
 
-const app = express();
-app.use(express.json());
-
-async function populateCartIds(ids) {
   await client.connect();
   const db = client.db("fsv-db");
-  return Promise.all(
-    ids.map((id) => db.collection("products").findOne({ id }))
-  );
+
+  const app = express();
+  app.use(express.json());
+
+  async function populateCartIds(ids) {
+    return Promise.all(
+      ids.map((id) => db.collection("products").findOne({ id }))
+    );
+  }
+
+  app.get("/products", async (req, res) => {
+    const products = await db.collection("products").find({}).toArray();
+    res.json(products);
+  });
+
+  app.get("/users/:userId/cart", async (req, res) => {
+    const user = await db
+      .collection("users")
+      .findOne({ id: req.params.userId });
+    const populatedCartItems = await populateCartIds(user.cartItems);
+    res.json(populatedCartItems);
+  });
+
+  app.get("/products/:productId", async (req, res) => {
+    const productId = req.params.productId;
+    const product = await db.collection("products").findOne({ id: productId });
+    res.json(product);
+  });
+
+  app.post("/users/:userId/cart", async (req, res) => {
+    const productId = req.body.id;
+    const userId = req.params.userId;
+
+    if (!productId) {
+      return res
+        .status(400)
+        .json({ error: "Missing product id in request body" });
+    }
+
+    await db.collection("users").updateOne(
+      { id: userId },
+      {
+        $addToSet: { cartItems: productId },
+      }
+    );
+
+    const user = await db.collection("users").findOne({ id: userId });
+
+    const populatedCartItems = await populateCartIds(user.cartItems);
+    res.json(populatedCartItems);
+  });
+
+  app.delete("/users/:userId/cart/:productId", async (req, res) => {
+    const productId = req.params.productId;
+    const userId = req.params.userId;
+
+    if (!productId) {
+      return res
+        .status(400)
+        .json({ error: "Missing product id in request body" });
+    }
+
+    await db.collection("users").updateOne(
+      { id: userId },
+      {
+        $pull: { cartItems: productId },
+      }
+    );
+
+    const user = await db.collection("users").findOne({ id: userId });
+
+    const populatedCartItems = await populateCartIds(user.cartItems);
+    res.json(populatedCartItems);
+  });
+
+  app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+  });
 }
 
-app.get("/products", async (req, res) => {
-  await client.connect();
-  const db = client.db("fsv-db");
-  const products = await db.collection("products").find({}).toArray();
-  res.json(products);
-});
-
-app.get("/users/:userId/cart", async (req, res) => {
-  await client.connect();
-  const db = client.db("fsv-db");
-  const user = await db.collection("users").findOne({ id: req.params.userId });
-  const populatedCartItems = await populateCartIds(user.cartItems);
-  res.json(populatedCartItems);
-});
-
-app.get("/products/:productId", (req, res) => {
-  const productId = req.params.productId;
-  const product = products.find((product) => product.id === productId);
-  res.json(product);
-});
-
-app.post("/cart", (req, res) => {
-  const productId = req.body.id;
-  if (!productId) {
-    return res
-      .status(400)
-      .json({ error: "Missing product id in request body" });
-  }
-  cartItems.push(productId);
-  const populatedCartItems = populateCartIds(cartItems);
-  res.json(populatedCartItems);
-});
-
-app.delete("/cart/:productId", (req, res) => {
-  const productId = req.params.productId;
-  cartItems = cartItems.filter((id) => id !== productId);
-  const populatedCartItems = populateCartIds(cartItems);
-  res.json(populatedCartItems);
-});
-
-app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
-});
+start();
