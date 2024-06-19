1f994348fe4a1d754dff8e87e8be81b6fa64a107
Adebola <adebola@poptin.com>
2024-06-19 15:57:11 +0100

    endpoint to get products, get cartItems, get single product, add product to cart, remove product from cart

--- src/server.js
+++ src/server.js
@@ -1,9 +1,41 @@
 import express from "express";
+import {
+  cartItems as cartItemsRaw,
+  products as productsRaw,
+} from "./temp-data";
+
+// so we can modify the external data
+let cartItems = cartItemsRaw;
+let products = productsRaw;
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/cart", (req, res) => {
+  res.json(cartItems);
+});
+
+app.get("/products/:productId", (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find((product) => product.id === productId);
+  res.json(product);
+});
+
+app.post("/cart", (req, res) => {
+  const productId = req.body.id;
+  const product = products.find((product) => product.id === productId);
+  cartItems.push(product);
+  res.json(cartItems);
+});
+
+app.delete("/cart/:productId", (req, res) => {
+  const productId = req.params.productId;
+  cartItems = cartItems.filter((product) => product.id !== productId);
+  res.json(cartItems);
 });
 
 app.listen(8000, () => {
